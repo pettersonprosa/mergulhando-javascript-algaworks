2a8233593fa85d0decbe40d205d7d94e021e67fd
petterson <petterson@email.com>
2025-07-16 10:01:43 -0300

    Criando objetos

---
+++ 02-03-objetos/script.js
@@ -0,0 +1,14 @@
+var person = {
+  name: 'Joao',
+  age: 20,
+  sex: 'male'
+}
+
+console.log(person);
+
+console.log(person.name)
+
+console.log(person.age)
+person.age = 35
+console.log(person.age)
+console.log(person);
