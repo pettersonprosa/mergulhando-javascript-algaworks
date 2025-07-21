23f8e35f302d8df59cc491c3d07e1b490c3faa80
petterson <petterson@email.com>
2025-07-21 09:28:07 -0300

    Trabalhando com template strings

---
+++ 03-11-template-strings/index.js
@@ -0,0 +1,10 @@
+const name = 'Joao';
+const age = 22;
+
+// Concatenação
+const sentence = 'Olá, meu nome é ' + name + ' e eu tenho '+ age + ' anos.'
+console.log(sentence)
+
+// Interpolação
+const sentence2 = `Olá, meu nome é ${name} e eu tenho ${age} anos`;
+console.log(sentence2)
